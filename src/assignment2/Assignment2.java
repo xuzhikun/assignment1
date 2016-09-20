09eddc3d6624d07fa0f8444db555c1c2164a29b3
xuzhikun <994849761@qq.com>
2016-09-20 14:51:25 -0700

    done

--- src/assignment2/Assignment2.java
+++ src/assignment2/Assignment2.java
@@ -1,3 +1,6 @@
+import java.lang.Integer;
+import java.lang.Math;
+import java.lang.String;
 import java.lang.System;
 
 /**
@@ -80,15 +83,47 @@ public class Assignment2 {
      * Write a method to determine whether a number is prime
      */
     public void isPrime(int n) {
+        String yes = "The number is a prime.";
+        String no = "The number is not a prime."
 
+        if (n == 2) {
+            System.out.println(yes);
+        }
+        if (n <= 1) {
+            System.out.println(no);
+        }
+        int sqrt = (int)Math.sqrt(n);
+        for (int i = 2; i <= sqrt; i++) {
+            if (a % i != 0) {
+                System.out.println(yes);
+            }
+            else {
+                System.out.println(no);
+            }
+        }
     }
 
     /**
      * Find N fibonacci numbers Fibonacci Number: f(N) = f(N-1)+f(N-2).
      * Typically, f(0)=f(1)=1.
      */
-    public int[] fibonacciNumber(int n) {
-        return null;
+    public static int[] fibonacciNumber(int n) {
+        int[] f = new int[n + 1];
+        if (n < 0) {
+            return null;
+        }
+        if (n == 0) {
+            return new int[]{1};
+        }
+        if (n == 1) {
+            return new int[]{1, 1};
+        }
+
+        f[0] = f[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            f[i] = f[i - 1] + f[i - 2];
+        }
+        return f;
     }
 
     /**
@@ -96,15 +131,42 @@ public class Assignment2 {
      * reversed. Given s = "hello", return "olleh".
      */
     public String reverseString(String s) {
-        return null;
+        char[] str = s.toCharArray();
+        int start = 0;
+        int end = s.length() - 1;
+        while (start < end) {
+            char temp = str[start];
+            str[start] = str[end];
+            str[end] = temp;
+            start++;
+            end--;
+        }
+        return new String(str);
     }
 
     /**
      * Given an array of nums, write a function to find the largest two integer.
      */
     public int[] findTheLargestTwo(int[] nums) {
-        return null;
+        if (nums.length <= 2 || nums == null) {
+            return nums;
+        }
+        int l1 = Integer.MIN_VALUE, l2 = Integer.MIN_VALUE;
+        int index1 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= l1) {
+                l1 = nums[i];
+                index1 = i;
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] <= l1 && i != index1) {
+                l2 = Math.max(l2, nums[i]);
+            }
+        }
+        return new int[]{l1, l2};
     }
+
 
     /**
      * Given an array nums, write a function to move all 0's to the end of it
@@ -113,6 +175,20 @@ public class Assignment2 {
      * should be [1, 3, 12, 0, 0].
      */
     public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        for (int i = index; i < nums.length; i++) {
+            nums[i] = 0;
+        }
 
     }
 
@@ -123,7 +199,12 @@ public class Assignment2 {
      * like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
      */
     public int addDigits(int n) {
-        return 0;
+        int sum = 0;
+        while (n % 10 != 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
     }
 
     /**
@@ -134,7 +215,27 @@ public class Assignment2 {
      * number.
      */
     public boolean isUgly(int n) {
-        return false;
-    }
+        if (n <= 0) {
+            return false;
+        }
+        if (n == 1) {
+            return true;
+        }
 
+        while (num >= 2) {
+            if (num % 2 == 0) {
+                num /= 2;
+            }
+            else if (num % 3 == 0) {
+                num /= 3;
+            }
+            else if (num % 5 == 0) {
+                num /= 5;
+            }
+            else {
+                return false;
+            }
+        }
+        return num == 1;
+    }
 }
